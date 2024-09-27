2955cad327049c0d92433070c168f510d6a2b971
saeye <saeye42@gmail.com>
2024-09-27 19:58:42 +0900

    model: user클래스, 크레딧 필드 수정

--- users/models.py
+++ users/models.py
@@ -6,12 +6,12 @@ class User(AbstractUser):
     riot_username = models.CharField(max_length=15, unique=True)  # 리그 오브 레전드 유저명
     riot_tag = models.CharField(max_length=15)  # 리그 오브 레전드 서버명
     discord_username = models.CharField(max_length=15, null=True, blank=True)  # 디스코드 유저명
-    discord_tag = models.CharField(max_length=15, null=True, blank=True)  # 디스코드 유저명
+    discord_tag = models.CharField(max_length=15, null=True, blank=True)  # 디스코드 태그명
     email = models.EmailField(unique=True)  
     date_joined = models.DateField(auto_now_add=True) 
     is_active = models.BooleanField(default=True)  
     profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
-    credit_info = models.CharField(max_length=25, null=True, blank=True)
+    credit_info = models.PositiveIntegerField(null=True, blank=True)  # 충전 금액
     introduction = models.TextField(null=True, blank=True)
     score = models.FloatField(default=0.0)
     is_notified = models.BooleanField(default=False)
@@ -23,3 +23,4 @@ class User(AbstractUser):
     
     class Meta:
         ordering = ['-date_joined']  # 가입일 기준 내림차순 정렬
+

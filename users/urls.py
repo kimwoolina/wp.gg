668be535c4dbf1b1601d27ec6fc4effb5745e568
saeye <saeye42@gmail.com>
2024-09-30 03:07:08 +0900

    featL: feat/users_signup 필요없는 코드 삭제

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CustomRegisterView.as_view(), name='register'),  # 기본 URL 설정
+    path('', views.CustomRegisterView.as_view(), name='register'), 
 ]

ba58f08b4a046dffa684c6d181f895b45e91dfe4
kimwoolina <rladnfls0500@gmail.com>
2024-09-30 21:52:08 +0900

    feat: 유저 검색 하여 유저 상세 정보 조회하는 기능 구현, User-Evaluations 1:1관계로 변경, UserSerializer 작성.

---
+++ users/serializers.py
@@ -0,0 +1,31 @@
+# serializers.py
+from rest_framework import serializers
+from .models import User, Evaluations, Positions
+from articles.models import Articles 
+
+
+class EvaluationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluations
+        fields = '__all__'
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Positions
+        fields = ['position_name']
+
+        
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Articles
+        fields = ['id', 'title', 'created_at', 'reviewer'] 
+
+class UserSerializer(serializers.ModelSerializer):
+    evaluations = EvaluationSerializer(read_only=True)
+    positions = PositionSerializer(many=True, read_only=True)
+    articles = ArticleSerializer(many=True, read_only=True) 
+
+    class Meta:
+        model = User
+        fields = ['username', 'riot_username', 'riot_tag', 'riot_tier', 'positions', 'evaluations', 'articles']

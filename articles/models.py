e2d90260434c60d092c165157aa1ddabe44fc682
kimwoolina <rladnfls0500@gmail.com>
2024-10-02 12:48:49 +0900

    feat: @receiver 사용 -  유저 평점 자동 계산 모델 작성(해당 유저의 평가 점수의 평균을 score에 평균을 저장하는 코드)

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 User = get_user_model()
 
@@ -22,22 +24,42 @@ class Articles(models.Model):
     comment = models.ManyToManyField(
         User, symmetrical=False, related_name="article_comments", through="Comments"
         )
-
-
+# receiver:특정 이벤트 발생 시 실행되는 함수(post_save: db 저장 후 실행 / post_delete: db 제거 후 실행, 
+#                                          sender: 어떤 모델에서 신호를 받을지)
+@receiver(post_save, sender=Articles)
+@receiver(post_delete, sender=Articles)
+def update_like_count(sender, instance, **kwargs):
+    #평가 대상 유저
+    user = instance.user
+    
+    # 해당 유저에 대한 모든 평가 가져오기
+    reviews = user.reviewees.all()
+    
+    # 유저에 대한 평가가 있을 경우 평균 점수 계산, 없으면 0으로 설정
+    if reviews.exist():
+        # articvle_score 필드의 평균값
+        user.score = reviews.aggregate(models.Avg('article_score'))['score__avg']
+        # reviews.aggregate(models.Avg('article_score'))가 딕셔너리 형태로 저장된다고 해서 테스트용 print
+        print(user.score)
+    else:
+        user.score = 0
+    user.save()
+    
+    
 class Article_images(models.Model):
     article = models.ForeignKey(
         Articles, on_delete=models.CASCADE, related_name="article_images"
         )
     created_at = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(upload_to="%Y/%m/%d", blank=True, null=True)
-
-
+    
+    
 class Likes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Articles, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
+    
+    
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Articles, on_delete=models.CASCADE)

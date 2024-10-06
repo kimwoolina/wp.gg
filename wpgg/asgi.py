07425f3208293a3cf2d1662aa957c70d5e129a6b
saeye <saeye42@gmail.com>
2024-10-07 04:59:11 +0900

    feat: 채팅 기능 관련 코드 수정

--- wpgg/asgi.py
+++ wpgg/asgi.py
@@ -1,13 +1,21 @@
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chats.consumers import ChatConsumer
-from django.urls import path
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from chats.consumers import ChatConsumer
+
 
 # 환경변수 설정
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wp.gg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wpgg.settings')
 
+application = get_asgi_application()
+
+# Django 초기화
+django.setup()
+
+# ASGI 애플리케이션 설정
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(

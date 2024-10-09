2b74a6d35b39b8fa6fcb3d9ab3303ff869d22225
saeye <saeye42@gmail.com>
2024-10-09 12:37:38 +0900

    feat: 채팅 (5000번 포트 아무리 죽여도 계속 사용중이라고 해서 5001번으로 설정 / 데이터 저장을 위해 Flask-SQLAlchemy 설치 / 프론트엔드 메시지 보내기 버튼 위치 오른쪽으로 옮김)

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 import jwt
 
+# Flask 애플리케이션 초기화
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# 사용자 정보를 저장
 users = {}
 
+# 메인 페이지 라우트
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# 로그인 엔드포인트
 @app.route('/login/', methods=['POST'])
 def login():
     data = request.json
@@ -22,10 +26,12 @@ def login():
         return jsonify({'token': token})
     return jsonify({'error': 'Invalid username'}), 400
 
+# WebSocket 연결 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
 
+# WebSocket 메시지 
 @socketio.on('message')
 def handle_message(data):
     token = data.get('sender')
@@ -38,9 +44,11 @@ def handle_message(data):
         except jwt.InvalidTokenError:
             print('Invalid token')
 
+# WebSocket 연결 해제 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
+# 5001서버 실행
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True)

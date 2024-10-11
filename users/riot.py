58821256e402934fd3b8c6c74a75b19874637cce
kimwoolina <rladnfls0500@gmail.com>
2024-10-12 01:37:43 +0900

    API: Riot API로 소환사 정보 가져오기 테스트 성공

--- users/riot.py
+++ users/riot.py
@@ -12,11 +12,13 @@ from wpgg.settings import RIOT_API_KEY
 api_key = RIOT_API_KEY
 riot_id = '캣티천사'
 tag_line = 'KR1'
+summoner_id = 'lynpeep'
 
 # 북미서버 테스트
 # riot_id = 'exsmiley'
 # tag_line = 'smile'
 
+# 유저네임, 유저태그로 puuid 얻기
 def get_puuid(api_key, riot_id, tag_line):
     encoded_riot_id = quote(riot_id)
     encoded_tag_line = quote(tag_line)
@@ -44,5 +46,61 @@ def get_puuid(api_key, riot_id, tag_line):
         print(f"Request failed: {e}")
 
 
+def get_profile_by_puuid(api_key, puuid):
+    
+    #한국 서버
+    url_profile = f"https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={api_key}"
+    
+    # 북미서버
+    #url_puuid = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{encoded_riot_id}/{encoded_tag_line}?api_key={api_key}"
+
+    try:
+        response_profile = requests.get(url_profile, timeout=10)  # 타임아웃 추가
+        print(f"Response Status Code: {response_profile.status_code}")  # 응답 코드 확인
+
+        if response_profile.status_code == 200:
+            data_profile = response_profile.json()
+            print(f"profile: {data_profile}")
+            return data_profile
+        else:
+            print(f"Error: Status Code {response_profile.status_code}")
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        
+
+def get_league_by_uid(api_key, uid):
+    
+    #한국 서버
+    url_league = f"https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{uid}?api_key={api_key}"
+    
+    try:
+        response_league = requests.get(url_league, timeout=10)  # 타임아웃 추가
+        print(f"Response Status Code: {response_league.status_code}")  # 응답 코드 확인
+
+        if response_league.status_code == 200:
+            data_league = response_league.json()
+            print(f"league: {data_league}")
+            return data_league
+        else:
+            print(f"Error: Status Code {response_league.status_code}")
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")  
+
+
+
+
+
 # 함수 호출
 get_puuid(api_key, riot_id, tag_line)
+
+
+puuid = 'eLG4Ar5xPfzO_lo7Ganx2dG5YZDxPpi48xuwmHb2YBFH3Ia1RsKJmVGqppIf84fOGFH4zTtMlkP43w'
+get_profile_by_puuid(api_key, puuid)
+
+uid = 'dFrpUMMl6KFwwQr9EUWcBk53vReJRE7LspCmW7IEqensew'
+get_league_by_uid(api_key, uid)
+

ba30bfa67ac18571c11cd911ccd8bc922c1c6f43
nyong <sptcnl@gmail.com>
2024-10-11 22:53:49 +0900

    fix: app name 추가, list / create분리 update: 리스트, 등록 페이지 url 추가

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from articles import views
 
+app_name = "articles"
 urlpatterns = [
-    path('', views.ArticleAPIView.as_view(), name='articles'), #리스트, 생성
+    path('', views.ArticleListView.as_view(), name='articles'), #리스트
+    path('create/', views.ArticleCreateView.as_view(), name='article'), #생성
     path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'), #상세
     path('comment/<int:pk>/', views.CommentAPIView.as_view(), name='comment'), #댓글
+
+    #FE
+    path('article-list-page/', views.article_list_page, name='list'),
+    path('create-page/', views.article_create_page, name='create'),
 ]

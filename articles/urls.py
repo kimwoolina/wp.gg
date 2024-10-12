e20fd00857830c1a5efc2194d1173e10693dcc13
nyong <sptcnl@gmail.com>
2024-10-13 00:55:12 +0900

    update: 주석 추가

--- articles/urls.py
+++ articles/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('comment/<int:pk>/', views.CommentAPIView.as_view(), name='comment'), #댓글
 
     #FE
-    path('article-list-page/', views.article_list_page, name='list'),
-    path('create-page/', views.article_create_page, name='create'),
+    path('article-list-page/', views.article_list_page, name='list'), #리스트
+    path('create-page/', views.article_create_page, name='create'), #생성
 ]

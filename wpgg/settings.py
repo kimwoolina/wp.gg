fbc2ccc59dbd6b99a7f2c3fc3dedb56fed8dcd99
kimwoolina <rladnfls0500@gmail.com>
2024-10-13 16:00:51 +0900

    feat: 디스코드 로그인 후 인증토큰 처리

--- wpgg/settings.py
+++ wpgg/settings.py
@@ -113,6 +113,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -141,9 +142,15 @@ WSGI_APPLICATION = 'wpgg.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # JWT 인증
+        # 'rest_framework.authentication.SessionAuthentication',        # 세션 인증 (옵션)
+        # 'rest_framework.authentication.BasicAuthentication',         # 기본 인증 (옵션)
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',  # 인증된 사용자만 접근
     ),
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),

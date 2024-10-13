e55c354ca6355b7834928d6504c3414c0732b8df
kimwoolina <rladnfls0500@gmail.com>
2024-10-14 05:22:31 +0900

    refact: articles 앱 관련 프론트엔드, 백엔드 분리

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,6 @@ from articles import views
 app_name = "articles"
 urlpatterns = [
     path('', views.ArticleAPIView.as_view(), name='articles'), #리스트,생성
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'), #상세
     path('comment/<int:pk>/', views.CommentAPIView.as_view(), name='comment'), #댓글
+    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'), #상세
 ]
